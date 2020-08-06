d43b5d52a6ca449117de938f5002a7d7d2bcda72
Guilherme de Alvarenga <gui.alva50@gmail.com>
2020-08-06 15:49:58 -0300

    Configurando o Prettier com o Eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
 import Head from 'next/head'
 
 type Props = {
-  title: string;
+  title: string
 }
 
-export default function Home({ title = "React Avançado!"} : Props) {
+export default function Home({ title = 'React Avançado!' }: Props) {
   return (
-      <Head>
-        <h1>
-          <a>{title}</a>
-        </h1>
-      </Head>
+    <Head>
+      <h1>
+        <a>{title}</a>
+      </h1>
+    </Head>
   )
 }
