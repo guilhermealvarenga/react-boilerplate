030b4681cc0f0618e98b33caea3a660011c2e76a
Guilherme de Alvarenga <gui.alva50@gmail.com>
2020-08-25 11:08:30 -0300

    Configurando Storybook e escrevendo stories

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { text, withKnobs } from '@storybook/addon-knobs'
+
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  decorators: [withKnobs]
+} as Meta
+
+export const Basic: Story = () => (
+  <Main
+    title={text('Title', 'React Avançado')}
+    description={text(
+      'Description',
+      'Typescript, ReactJS, NextJS e Styled Components'
+    )}
+  />
+)
