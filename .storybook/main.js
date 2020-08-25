790686bb32d5a23769ad38c2bbc02980b1703f5c
Guilherme de Alvarenga <gui.alva50@gmail.com>
2020-08-25 13:32:29 -0300

    Usando Storybook Essentials e Controls

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,4 @@
 module.exports = {
-  "stories": [
-    "../src/components/**/stories.mdx",
-    "../src/components/**/stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-knobs"
-  ]
+  "stories": ["../src/components/**/stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-essentials"]
 }
